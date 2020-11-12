d7ac0dc13573a38431b1898c5039df5f59aa8d34
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-11-12 11:41:48 -0600

    feat: show error message when registration error

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -8,12 +8,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO: show the registration form
+       //makes so that the error message doesn't recur in session after fixed
+        HttpSession session = request.getSession();
+        String errorMessage = (String).getAttribute("registerError");
+        if(errorMessage != null){
+            request.setAttribute("registerError", errorMessage);
+            session.removeAttribute("registerError");
+        }
+
         request. getRequestDispatcher("/WEB-INF/register.jsp").forward(request,response);
     }
 
@@ -31,7 +40,9 @@ public class RegisterServlet extends HttpServlet {
                 || confirm.isEmpty()
                 || (!password.equals(confirm));
         if(inputHasErrors){
+            request.getSession().setAttribute("registerError", "There was an error with your registration.");
             response.sendRedirect("/register");
+            return;
         }
         // TODO: create a new user based off of the submitted information
 
--- src/main/webapp/WEB-INF/register.jsp
+++ src/main/webapp/WEB-INF/register.jsp
@@ -6,6 +6,8 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
         <jsp:include page="/WEB-INF/partials/head.jsp">
@@ -17,6 +19,11 @@
 
     <div class="container">
         <h1>Please fill in your information.</h1>
+        <c:if test="${sessionScope.registerError !=null}">
+            <div class="alert alert-danger" role="alert">
+                ${requestScope.registerError}
+            </div>
+        </c:if>
         <form action="/register" method="post">
             <div class ="form-group">
                 <label for="username">Usernaem</label>
